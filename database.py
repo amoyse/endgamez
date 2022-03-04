4748d5b07c69b3c98af735662307f9d7c0dfed7a
komododragonn <securemoyse127@protonmail.com>
2022-03-04 08:20:09 +0000

    Set up database

---
+++ database.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+conn = sqlite3.connect("database.db")
+c = conn.cursor()
+
+c.execute(""" CREATE TABLE IF NOT EXISTS USER (
+            ENDGAME_ID        INTEGER PRIMARY KEY AUTOINCREMENT,
+            NAME              TEXT NOT NULL,
+            FEN               TEXT NOT NULL,
+            ABILITY           TEXT NOT NULL,
+            MOVES             INTEGER
+            ); """)
+print("Table created successfully")
